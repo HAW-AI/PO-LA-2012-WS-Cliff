2a2484bc83fd97ebf7d483c25318b002e6f7918b
Fenja <Fenja@H>
2012-10-29 14:00:58 +0100

    more GUI

--- src/haw/po/la/cliff/GUI.java
+++ src/haw/po/la/cliff/GUI.java
@@ -1,10 +1,15 @@
 package haw.po.la.cliff;
 
+/**
+ * 
+ * @author Fenja Harbke
+ *
+ */
+
 import java.awt.Color;
 import java.awt.Frame;
 import java.awt.Graphics;
 import java.applet.Applet;
-import java.util.ArrayList;
 import java.util.List;
 
 @SuppressWarnings("serial")
@@ -22,41 +27,37 @@ public class GUI extends Applet{
 	public int height = 3;
 	public Position startField = new Position(0,0);
 	public Position endField = new Position(width-1,0);
-	public List<Position> cliffList = new ArrayList<Position>();
+	public List<Position> cliffList;
+	public Position agentPos;
 	
-//	public GUI(Position startField, Position endField, List<Position> cliffList){
-//		this.startField = startField;
+	public GUI(Environment env){
+		g = getGraphics();
+//		this.startField = env.startField;
 //		this.endField = endField;
 //		this.cliffList = cliffList;
-//		frame = new Frame();
-//		frame.setResizable(true);
-//		frame.add(this);
-//		frame.pack();
-//		frame.setSize(width*fieldSize+(2*startx),height*fieldSize+(2*starty));
-//		init();
-//		frame.setVisible(true);	
+//		this.width = env.envSizeX;
+//		this.height = env.envSizeY;
+//		this.agentPos = env.envAgentValue;
+		frame = new Frame();
+		frame.setResizable(true);
+		frame.add(this);
+		frame.pack();
+		frame.setSize(width*fieldSize+(2*startx),height*fieldSize+(2*starty));
+		draw(g);
+		frame.setVisible(true);	
+	}
+	
+//	public void init(){
+//		//g = getGraphics();
+//		//repaint();
+//		//while(true){
+//			//drawAgent();
+//		//}
 //	}
 	
-	public void init(){
-		g = getGraphics();
-		//repaint();
-		//while(true){
-			//drawAgent();
-		//}
-	}
-	
-	public void drawAgent(){
-		Position pos = getAgentPos();
-		g.setColor(Color.red);
-		g.fillOval(startx+((fieldSize+1)*(pos.x()-1)+1), starty+((fieldSize+1)*(pos.y()-1)+1), fieldSize-1, fieldSize-1);
-	}
-	
-	public Position getAgentPos(){
-		return new Position(2,3);
-	}
-	
-	public void paint (Graphics g){
+	private void draw (Graphics g){
 		this.g = g;
+		//Grid
 		g.setColor(Color.black);
 		g.drawLine(startx, starty, startx, starty+height*(fieldSize+1)); //waagerecht
 		g.drawLine(startx, starty, startx+width*(fieldSize+1), starty); //senkrecht
@@ -66,15 +67,31 @@ public class GUI extends Applet{
 		for(int i = 1; i <= width; i++){
 			g.drawLine(startx+((fieldSize+1)*i), starty, startx+((fieldSize+1)*i), starty+height*((fieldSize+1)));
 		}
+		//start & end field
 		g.setColor(Color.lightGray);
 		g.fillRect(startx+((fieldSize+1)*startField.x()+1), starty+((fieldSize+1)*startField.y()+1), fieldSize, fieldSize);
 		g.fillRect(startx+((fieldSize+1)*endField.x()+1), starty+((fieldSize+1)*endField.y()+1), fieldSize, fieldSize);
-		
+		//cliff
+		if(cliffList!=null){
+			g.setColor(Color.black);
+			for(Position p : cliffList){
+				g.fillRect(startx+((fieldSize+1)*p.x()+1), starty+((fieldSize)*p.y()+1), fieldSize, fieldSize);
+			}
+		}
 		drawAgent();
 	}
+
+	private void drawAgent(){
+		g.setColor(Color.red);
+		g.fillOval(startx+((fieldSize+1)*(agentPos.x()-1)+1), starty+((fieldSize+1)*(agentPos.y()-1)+1), fieldSize-1, fieldSize-1);
+	}
 	
-    public void render(Environment env, Position pos) {
-        // do smth
-        // or change the interface
+    public void render(Position agentPos) {
+    	if (this.agentPos != agentPos){
+    		this.agentPos = agentPos;
+    		drawAgent();
+    	}else{
+    		//overpaint old pos
+    	}
     }
 }
