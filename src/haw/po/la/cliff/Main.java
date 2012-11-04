9e0f80dfb6308befb66563a91ee8eb37b3a9ec58
Till Theis <theis.till@gmail.com>
2012-11-04 23:48:05 +0100

    remove commented code

--- src/haw/po/la/cliff/Main.java
+++ src/haw/po/la/cliff/Main.java
@@ -21,36 +21,6 @@ public class Main {
 		Thread.sleep(300);//wait for x secs
 		sim.step();
 		}
-		
-//		System.out.println("CLIFF\n");
-//
-//		// TEST ENVIRONMENT
-//		Environment env = new Environment();
-//		Position startPos = new Position(0, 0);
-//		//1 step
-//		System.out.println("startPos:(" + startPos.x() + " , " + startPos.y()+ ") , UP");
-//		List<Direction> dirList = env.getPossibleDirections(startPos);
-//
-//			System.out.println("dirList: UP="+ (dirList.contains(Direction.UP))+"\tRIGHT="+ (dirList.contains(Direction.RIGHT)+"\tDOWN="+(dirList.contains(Direction.DOWN)+"\tLEFT="+(dirList.contains(Direction.LEFT)))));
-//	
-//		Pair<Position, Double> reward = env.nextState(startPos, Direction.UP);
-//		System.out.println("newPos: (" + reward.first.x() + " , "+ reward.first.y() + ")\treward: " + reward.second);
-//		startPos=reward.first;
-//		System.out.println("\n");
-//		//2 step
-//		System.out.println("Pos:(" + startPos.x() + " , " + startPos.y()+ ") , UP");
-//		dirList.clear();
-//		dirList = env.getPossibleDirections(startPos);
-//		
-//			System.out.println("dirList: UP="+ (dirList.contains(Direction.UP))+"\tRIGHT="+ (dirList.contains(Direction.RIGHT)+"\tDOWN="+(dirList.contains(Direction.DOWN)+"\tLEFT="+(dirList.contains(Direction.LEFT)))));
-//		
-//		reward=null;
-//		reward = env.nextState(startPos, Direction.UP);
-//		System.out.println("newPos: (" + reward.first.x() + " , "+ reward.first.y() + ")\treward: " + reward.second);
-//		System.out.println("\n");
-		
-		
-		
 	}
 
 }
