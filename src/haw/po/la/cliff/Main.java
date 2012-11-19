ceca7394f76607c83e98327845dc15db2e842072
Till Theis <theis.till@gmail.com>
2012-11-19 09:58:03 +0100

    cleaning up

--- src/haw/po/la/cliff/Main.java
+++ src/haw/po/la/cliff/Main.java
@@ -1,30 +1,12 @@
 package haw.po.la.cliff;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Main {
 
 	public static void main(String[] args) throws Exception {
-//		List<Position> cliffList = new ArrayList<Position>();
-//		cliffList.add(new Position(1,0));
-//		cliffList.add(new Position(2,0));
-//		cliffList.add(new Position(3,0));
-//		cliffList.add(new Position(4,0));
-//		cliffList.add(new Position(5,0));
-//		cliffList.add(new Position(6,0));
-//		
-//		//EnvironmentImpl env = new EnvironmentImpl(3,8,new Position(0,0), new Position(7,0),cliffList);
 		EnvironmentImpl env = new EnvironmentImpl(3,5);
 		SimulationImpl sim = new SimulationImpl(env);
 		GuiImpl gui = new GuiImpl(env, sim);
 		sim.addObserver(gui);
-//		
-//		SimulationImpl sim = new SimulationImpl(env);
-//		for(int i = 0; i < 1000000; i++){
-//		Thread.sleep(5);//wait for x secs
-//		sim.step();
-//		}
 	}
 
 }
