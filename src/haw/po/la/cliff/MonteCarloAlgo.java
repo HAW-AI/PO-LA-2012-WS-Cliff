5a714142a22e89f3385da075167a8bf68f21fa44
bjoernKulas <bjoern.kulas@haw-hamburg.de>
2012-11-25 21:04:01 +0100

    Monte Carlo simplified, add policy output

--- src/haw/po/la/cliff/MonteCarloAlgo.java
+++ src/haw/po/la/cliff/MonteCarloAlgo.java
@@ -41,7 +41,7 @@ public class MonteCarloAlgo implements Algo {
 	private int win;
 
 	/**
-	 * default epsilon = 0.4
+	 * default epsilon = 0.3
 	 * 
 	 * @param env
 	 *            Environment
@@ -54,16 +54,173 @@ public class MonteCarloAlgo implements Algo {
 		this.epsilon = epsilon;
 		this.env = env;
 		this.explore = this.epsilon / Direction.values().length;
-		this.exploit = (1 - this.epsilon)
-				+ (this.epsilon / Direction.values().length);
+		this.exploit = getExploit();
 		this.policyInit = 1.0 / Direction.values().length;
 		
 	this.lost=0;
-	this.win =0;
+	this.win=0;
 		
 		initArrays();
 	}
 
+	
+
+	/**
+	 * use this method before a new episode is starting
+	 */
+	@Override
+	public void startEpisode() {
+		this.pairList = new ArrayList<Pair<Position, Direction>>();
+		this.statelist = new ArrayList<Position>();
+		System.out.println("\n\n\t\tNEW EPSIODE\n\n");
+	}
+
+	/**
+	 * use this method to declare the end of an episode
+	 */
+	@Override
+	public void endEpisode() {
+		this.episodeCounter++;
+		System.out.println("\nPOLICY\n");
+		showPolicy();
+		System.out.println("\n\n\t\tEND EPSIODE " + this.episodeCounter	+ "\n\tWIN: "+this.win+"\tLOST: "+this.lost+"\tW/L-RATIO: "+(double)((double)this.win/(double)this.lost)+"\n");
+		
+		String output="";
+		for(int i=0;i<this.statelist.size();i++){
+			output= output+"   ("+statelist.get(i).x()+","+statelist.get(i).y()+") ";
+		}
+		System.out.println("\nSTATES: "+output+"\n");
+		
+		
+		
+		//for each s in episode
+		for(int s=0;s<this.statelist.size();s++){
+			int[] actionsEqualsArgMaxA=new int[Direction.values().length];
+			int actionsEqualsArgMaxAIndex=0;
+			Position state = this.statelist.get(s);
+			//arg_max_a Q(s)
+				//Q(s)
+			double[] qActions=this.q[state.x()][state.y()].clone();
+			Arrays.sort(qActions);
+			//arg_MAX_A		
+			int qActionIndex=qActions.length-1;
+			//ARG_MAX_A	
+			double arg_max_a=qActions[qActionIndex];
+			//for all a elements of A(s)
+			for(int d=0;d<Direction.values().length;d++){
+				double policyActions = this.q[state.x()][state.y()][d];
+				
+				//Policy(s,a)  <-- if(a==arg_max_A) this.policy[state.x()][state.y()][d]==this.exploit 
+				// 					else			this.policy[state.x()][state.y()][d]==this.explore
+				if(arg_max_a==policyActions){
+					//save the index of actions, for all actions that fulfill a==arg_max_A
+					actionsEqualsArgMaxA[actionsEqualsArgMaxAIndex]=d;
+					actionsEqualsArgMaxAIndex++;
+				}else{
+					this.policy[state.x()][state.y()][d]=this.explore;
+				}
+			}
+			//set random which action, that fulfill a==arg_max_a, is exploit , others explore  
+			int numActionsEqualsArgMaxA=actionsEqualsArgMaxAIndex;
+			int random = (int) (Math.random()*numActionsEqualsArgMaxA);
+			for(int i=0;i<numActionsEqualsArgMaxA;i++){
+				if(i==random){
+					this.policy[state.x()][state.y()][actionsEqualsArgMaxA[i]]=this.exploit;
+				}else{
+					this.policy[state.x()][state.y()][actionsEqualsArgMaxA[i]]=this.explore;
+				}
+			}
+		
+		}
+
+		System.out.println("NEW POLICY");
+		showPolicy();
+		
+	}
+
+	@Override
+	public Direction getDirection(Position pos) {
+		Direction returnDir = null;
+		double rand = Math.random();
+		double policyValue=0.0;
+		int dirIndex=0;
+		
+		for(int d=0;d<Direction.values().length;d++){
+			policyValue+=this.policy[pos.x()][pos.y()][d];
+			if(policyValue>rand){
+				dirIndex=d;
+				d=Direction.values().length;
+				
+			}
+		}
+		
+		returnDir=Direction.values()[dirIndex];
+		return returnDir;
+	}
+
+	@Override
+	public void learn(Position initialPos, Direction dir,Position resultingPos, Double reward) {
+		Pair<Position, Direction> newPair = new Pair<Position, Direction>(initialPos, dir);
+		
+		if(this.env.getFinishPosition().equals(resultingPos)){
+			this.win++;
+		}
+		if(this.env.getCliffPositions().contains(resultingPos)){
+			this.lost++;
+		}
+		
+		// for each pair (s,a) in episode
+		// first occurrence
+		if (!this.pairList.contains(newPair)) {
+			this.pairList.add(newPair);
+		
+			// Append r to returns(s,a)
+			this.returns[initialPos.x()][initialPos.y()][dir.ordinal()].add(reward);
+			
+			//Q(s,a)<--avg(Returns(s,a))		?????????unhelpfully avg= sum / numOfSum  : -4 / 4.....all times -1
+			this.q[initialPos.x()][initialPos.y()][dir.ordinal()]=getAvg(this.returns[initialPos.x()][initialPos.y()][dir.ordinal()]);
+		}
+		
+		// save all s ---> for each s in episode 			??????initPos  or resultingPos
+		Position newState = new Position(initialPos.x(), initialPos.y());
+		if (!this.statelist.contains(newState)) {
+			this.statelist.add(newState);
+		}
+	}
+	
+	/*
+	 * -----------------HELPER-------------------------
+	 */
+
+	/**
+	 * get average value over all values of arrayList
+	 * 
+	 * @param arrayList
+	 *            an arrayList contains value of type double
+	 * @return an average value of all values are in parameter arrayList
+	 */
+	private double getAvg(ArrayList<Double> arrayList) {
+		double avg = 0.0;
+	
+		for (Double rewardsOfStateActionPair : arrayList) {
+			avg += rewardsOfStateActionPair;
+		}
+
+		avg = avg / arrayList.size();
+		return avg;
+	}
+
+
+
+
+	
+	private double getExploit() {
+		double expl =0.001+(1 - this.epsilon)+ (this.epsilon / Direction.values().length);
+		int exploitValue =(int) (1000*expl);
+		return (double)exploitValue/1000;
+	}
+
+	
 	@SuppressWarnings("unchecked")
 	/**
 	 * create and initialize the arrays: q, returns and policy
@@ -86,248 +243,15 @@ public class MonteCarloAlgo implements Algo {
 			}
 		}
 	}
-
-	/**
-	 * use this method before a new episode is starting
-	 */
-	@Override
-	public void startEpisode() {
-		this.pairList = new ArrayList<Pair<Position, Direction>>();
-		this.statelist = new ArrayList<Position>();
-		System.out.println("\n\n\t\tNEW EPSIODE\n\n");
+	
+	
+	private void showPolicy() {
+		for(int y=0;y<this.policy[0].length;y++){
+			System.out.println("-------------------------------------------------------------------------------------------------------------------------");
+			System.out.println("\t"+this.policy[0][y][2]+"\t\t|\t"+this.policy[1][y][2]+"\t\t|\t"+this.policy[2][y][2]+"\t\t|\t"+this.policy[3][y][2]+"\t\t|\t"+this.policy[4][y][2]+"\t\t|");
+			System.out.println(""+this.policy[0][y][0]+"\t (0,"+y+")\t"+this.policy[0][y][1]+"\t|"+this.policy[1][y][0]+"\t (1,"+y+")\t"+this.policy[1][y][1]+"\t|"+this.policy[2][y][0]+"\t (2,"+y+")\t"+this.policy[2][y][1]+"\t|"+this.policy[3][y][0]+"\t (3,"+y+")\t"+this.policy[3][y][1]	+"\t|"+	this.policy[4][y][0]+"\t (4,"+y+")\t"+this.policy[4][y][1]+"\t|");
+			System.out.println("\t"+this.policy[0][y][3]+"\t\t|\t"+this.policy[1][y][3]+"\t\t|\t"+this.policy[2][y][3]+"\t\t|\t"+this.policy[3][y][3]+"\t\t|\t"+this.policy[4][y][3]+"\t\t|");
+		}
+		System.out.println("-------------------------------------------------------------------------------------------------------------------------");
 	}
-
-	/**
-	 * use this method to declare the end of an episode
-	 */
-	@Override
-	public void endEpisode() {
-		
-		if(this.episodeCounter>=400){
-			this.epsilon=0.05;
-		}
-		
-		
-		this.episodeCounter++;
-		System.out.println("\n\n\t\tEND EPSIODE " + this.episodeCounter
-				+ "\n\tWIN: "+this.win+"\tLOST: "+this.lost+"\tW/L-RATIO: "+(double)(((double)this.win-(double)this.lost)/this.episodeCounter)+"\n");
-		
-
-		double[] sortedActionEvaluation = new double[4];
-
-		// for each pair (s,a) in episode
-		for (int i = 0; i < this.pairList.size(); i++) {
-
-			Position initialPos = new Position(	this.pairList.get(i).first().x(), this.pairList.get(i)	.first().y());
-			Direction dir = this.pairList.get(i).second();
-
-			// c) pre work: save each state in episode
-			if (!this.statelist.contains(initialPos)) {
-				this.statelist.add(initialPos);
-			}
-
-			// B) set Q(s,a) <-- average(returns(s,a))
-			this.q[initialPos.x()][initialPos.y()][dir.ordinal()] = getAvg(this.returns[initialPos
-					.x()][initialPos.y()][dir.ordinal()]);
-
-		}
-
-		// C) for each state in episode:
-		for (int i = 0; i < this.statelist.size(); i++) {
-
-			// for each s in the episode
-			// save action evaluations of q(s,a) for this state
-			double[] actionEvaluations = this.q[this.statelist.get(i).x()][this.statelist.get(i).y()];
-
-			// array clone
-			sortedActionEvaluation = actionEvaluations.clone(); 
-
-			// sort the cloned array of action evaluations
-			Arrays.sort(sortedActionEvaluation);
-
-			// save  highest  evaluationValue
-			double bestActionEvaluation = sortedActionEvaluation[3];
-
-			
-			//create an array, where the index of actions can be saved
-			int[] actionIndex = new int[4];
-			// initialize actionIndex-Array with -1, cause -1 is no index 
-			for (int a = 0; a < actionIndex.length; a++) {
-				actionIndex[a] = -1;
-			}
-
-			// set Policy: explore probability
-			int indexOfActionIndex = -1;
-			for (int a = 0; a < actionEvaluations.length; a++) {
-				
-				//check which evaluation is not the best evaluation
-				if (this.q[this.statelist.get(i).x()][this.statelist.get(i).y()][a] != bestActionEvaluation) {
-					// policy(s,a) <- epsilon/A(s), if a!=a*
-					this.policy[this.statelist.get(i).x()][this.statelist.get(i).y()][a] = this.explore;
-				} else {
-					indexOfActionIndex++;
-					actionIndex[indexOfActionIndex] = a;// save index of best rated action in actionIndex-Array
-					
-				}
-			}
-
-			if (indexOfActionIndex != -1) {// index of one or more better rated actions saved
-
-				int numOfBestActions = indexOfActionIndex+1;
-		
-				// create an array where the only the index of best rated actions will be saved
-				int[] bestActionIndex = new int[numOfBestActions];
-
-				
-				//save the index of the best rated action
-				bestActionIndex[0]=actionIndex[indexOfActionIndex];
-				
-				
-				//check if only one index of best rated action saved
-				if (numOfBestActions == 1) {
-					this.policy[this.statelist.get(i).x()][this.statelist.get(i).y()][bestActionIndex[0]] = this.exploit;
-				} else {// more than one best rated action saved
-					
-					
-					
-					int indexbestActionArray = 0;
-
-					for (int j = 0; j < actionIndex.length; j++) {
-						//save the index of the best rated actions
-						if (actionIndex[j] != -1) {
-							bestActionIndex[indexbestActionArray] = actionIndex[j];
-							indexbestActionArray++;
-						}
-					}
-					
-					// set randomNumber to choose one of the best rated actions 
-					int randNumber = (int) (Math.random() * numOfBestActions);
-
-					
-					//debug
-					if(randNumber>=numOfBestActions){
-						System.out.println("ERROR 3");
-					}
-					
-					
-					for (int j = 0; j < bestActionIndex.length; j++) {
-						if (randNumber == j) {
-							this.policy[this.statelist.get(i).x()][this.statelist.get(i).y()][bestActionIndex[j]] = this.exploit;
-						} else {
-							this.policy[this.statelist.get(i).x()][this.statelist.get(i).y()][bestActionIndex[j]] = this.explore;
-						}
-
-					}
-				}
-
-			}
-					
-		}
-
-		this.pairList = null;
-		this.statelist = null;
-
-	}
-
-	@Override
-	public void learn(Position initialPos, Direction dir,
-			Position resultingPos, Double reward) {
-		Pair<Position, Direction> firstPair = new Pair<Position, Direction>(
-				initialPos, dir);
-		
-		
-		//if(resultingPos==this.env.getFinishPosition()){
-			if(reward==20.0){
-			this.win++;
-		}
-		if(this.env.getCliffPositions().contains(resultingPos)){
-			this.lost++;
-		}
-		
-		
-		// for each pair (s,a) in episode
-		// first occurrence
-		if (!this.pairList.contains(firstPair)) {
-			this.pairList.add(firstPair);
-			// Append r to returns(s,a)
-			this.returns[initialPos.x()][initialPos.y()][dir.ordinal()]
-					.add(reward);
-//			System.out.println("\t\t\nFROM: "+initialPos+"\t"+dir+"\tto: "+resultingPos+"\t get REWARD: " + reward);
-		}
-	}
-
-	@Override
-	public Direction getDirection(Position pos) {
-		Direction returnDir = null;
-		double rand = Math.random();
-		
-		//zu x prozent trifft exploit, daher exploit zuerst mit rand pruefen
-		double[] actionArray = new double [Direction.values().length];
-
-		//create and sort array  to find exploit easy
-		actionArray = this.policy[pos.x()][pos.y()];
-		Arrays.sort(actionArray);
-		
-		//set best
-		double bestVal=actionArray[3];
-		
-		if(rand<bestVal){//take exploit
-			for(int a=0;a<Direction.values().length;a++){
-				//find exploit direction
-				if(this.policy[pos.x()][pos.y()][a]==bestVal){
-					//set exploit direction 
-					returnDir=Direction.values()[a];
-					//break the loop
-					a=Direction.values().length;
-				}
-			}
-		}else{
-			int count=0;
-			while(returnDir==null){
-				
-				int a= (int)(Math.random()*4);
-				
-				//find first explore direction
-				if(this.policy[pos.x()][pos.y()][a]!=bestVal){
-					//set explore direction 
-					returnDir=Direction.values()[a];
-					//break the loop
-					a=Direction.values().length;
-				}
-				
-				if(count==10){
-					//set explore direction 
-					returnDir=Direction.values()[a];
-				}
-				count++;
-			}
-		}
-
-		if(returnDir==null){
-			System.out.println("getDir  --> NULL");
-		}
-		
-		return returnDir;
-	}
-
-	/**
-	 * get average value over all values of arrayList
-	 * 
-	 * @param arrayList
-	 *            an arrayList contains value of type double
-	 * @return an average value of all values are in parameter arrayList
-	 */
-	private double getAvg(ArrayList<Double> arrayList) {
-		double avg = 0.0;
-		int counter = 0;
-
-		for (Double rewardsOfStateActionPair : arrayList) {
-			avg += rewardsOfStateActionPair;
-			counter++;
-		}
-
-		avg = avg / counter;
-		return avg;
-	}
-
 }
